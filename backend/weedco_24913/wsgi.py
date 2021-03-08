f801d26e4ad0b97b6d1c86fba3b44d3dbbcd3289
Crowdbotics <team@crowdbotics.com>
2021-03-08 15:52:59 +0000

    Initial commit.

---
+++ backend/weedco_24913/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for weedco_24913 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weedco_24913.settings')
+
+application = get_wsgi_application()
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://weedco-24913.botics.co",
+  defaultTimeout: 5000
+};
